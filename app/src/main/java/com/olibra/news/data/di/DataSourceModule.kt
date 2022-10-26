e551140a84a1f19a2d9b348e2e17321c1bf2822a
gianebinotto <giane.alessio@gmail.com>
2022-10-26 00:07:51 -0300

    feat: add datasource dependency injections

---
+++ app/src/main/java/com/olibra/news/data/di/DataSourceModule.kt
@@ -0,0 +1,32 @@
+package com.olibra.news.data.di
+
+import com.olibra.news.data.api.datasource.ArticleRemoteDataSource
+import com.olibra.news.data.api.datasource.ArticleRemoteDataSourceImpl
+import com.olibra.news.data.local.datasource.ArticleLocalDataSource
+import com.olibra.news.data.local.datasource.ArticleLocalDataSourceImpl
+import com.olibra.news.data.local.datasource.ArticleReadLocalDataSource
+import com.olibra.news.data.local.datasource.ArticleReadLocalDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+    @Binds
+    abstract fun bindArticleRemoteDataSource(
+        articleRemoteDataSource: ArticleRemoteDataSourceImpl
+    ): ArticleRemoteDataSource
+
+    @Binds
+    abstract fun bindArticleLocalDataSource(
+        articleLocalDataSource: ArticleLocalDataSourceImpl
+    ): ArticleLocalDataSource
+
+    @Binds
+    abstract fun bindArticleReadLocalDataSource(
+        articleReadLocalDataSource: ArticleReadLocalDataSourceImpl
+    ): ArticleReadLocalDataSource
+}
