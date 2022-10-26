e7cf3ffd97c91cc4452ed97a0211d786bc10f2fd
gianebinotto <giane.alessio@gmail.com>
2022-10-25 23:44:59 -0300

    change: article read repository interface

--- app/src/main/java/com/olibra/news/domain/repository/ArticleReadRepository.kt
+++ app/src/main/java/com/olibra/news/domain/repository/ArticleReadRepository.kt
@@ -1,9 +1,10 @@
 package com.olibra.news.domain.repository
 
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface ArticleReadRepository {
-    fun getAll(): Single<String>
-    fun add(articleId: String)
-    fun remove(articleId: String)
+    fun getArticlesRead(): Single<List<String>>
+    fun addArticleRead(articleId: String): Completable
+    fun removeArticleRead(articleId: String): Completable
 }
