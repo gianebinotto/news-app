b198e8790b783c2f9b6b2c0d48eadabc71aebbc8
gianebinotto <giane.alessio@gmail.com>
2022-10-26 00:07:28 -0300

    feat: add mapper dependency injections

---
+++ app/src/main/java/com/olibra/news/data/di/MapperModule.kt
@@ -0,0 +1,23 @@
+package com.olibra.news.data.di
+
+import com.olibra.news.data.api.mapper.ArticleRemoteMapper
+import com.olibra.news.data.local.mapper.ArticleLocalMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MapperModule {
+
+    @Provides
+    fun provideArticleRemoteMapper(): ArticleRemoteMapper {
+        return ArticleRemoteMapper()
+    }
+
+    @Provides
+    fun provideArticleLocalMapper(): ArticleLocalMapper {
+        return ArticleLocalMapper()
+    }
+}
